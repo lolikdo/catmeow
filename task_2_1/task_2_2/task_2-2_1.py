b326f599eedf6176936a74d4b7d678655e56adf2
Софья Барановская <baranovskaya.sk@edu.spbstu.ru>
2026-02-20 18:39:52 +0300

    Add files via upload

---
+++ task_2_1/task_2_2/task_2-2_1.py
@@ -0,0 +1,4 @@
+course_name = "english language"
+current_course_number = "3"
+number_of_completed_tasks = "23"
+print(f"Course: {course_name}, stage: {current_course_number}, completed tasks: {number_of_completed_tasks}")
