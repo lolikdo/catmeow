38ca8934693df2e7ceacbc173520ad3fcc5a3984
Софья Барановская <baranovskaya.sk@edu.spbstu.ru>
2026-02-25 12:41:26 +0300

    Delete task_2_3 directory

--- task_2_3/task_2-3_recipe.py
+++
@@ -1,8 +0,0 @@
-medium_name = input("Введите название питательной среды:")
-agricultural_concentration = input("Введите концетрацию агара")
-sterilization_temmperature = input("Введите температуру стерилизации")
-with open("recipe.txt", "w", encoding="utf-8") as file:
-    file.write(medium_name + "\n")
-    file.write(f" Концентрация агара: {agricultural_concentration}%\n")
-    file.write(f" Температура стерилизации:: {sterilization_temmperature}\n")
-print("\nФайл 'recipe.txt' успешно сформирован")
--- task_2_3/task_2-3_sensor.py
+++
@@ -1,5 +0,0 @@
-operator_name = input("Введите имя оператора:")
-pressure_value = input("Введите текущее значение датчика давления:")
-with open("sensor_log.txt", "w", encoding="utf-8") as file:
-  file.write(f"{operator_name}\t{pressure_value}\n")
-print("Данные успешно сохранены в sensor_log.txt")
