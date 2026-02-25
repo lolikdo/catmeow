0f706f5683242b3ac4a948634b58cfee9a8bc34f
Софья Барановская <baranovskaya.sk@edu.spbstu.ru>
2026-02-25 12:41:46 +0300

    Delete task_2_1 directory

--- task_2_1/task_2_2/task_2-2_3.py
+++
@@ -1,6 +0,0 @@
-device_name = "аэрогриль"
-inventory_number = "INV-2024-156" 
-state = "исправен"
-quantity = "23"
-print("Название\tИнвентарный номер\tСостояние\tКоличество")
-print(device_name, inventory_number, state, quantity, sep="\t\t" ) 
