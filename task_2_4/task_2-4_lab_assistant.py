8668c85ca5f9da9f7f1c9498b8de24acc1a0aeec
Софья Барановская <baranovskaya.sk@edu.spbstu.ru>
2026-02-25 12:41:17 +0300

    Delete task_2_4 directory

--- task_2_4/task_2-4_lab_assistant.py
+++
@@ -1,9 +0,0 @@
-volume = float(input("Введите нужный объем раствора (мл): "))
-salt_mass = volume * 0.009
-with open("recipe.txt", "w", encoding="utf-8") as file:
-    file.write("ОТЧЕТ ПО ПРИГОТОВЛЕНИЮ:\n")
-    file.write("-----------------------\n")
-    file.write(f"Общий объем:\t{volume:.2f} мл\n")
-    file.write(f"Масса соли:\t{salt_mass:.2f} г\n")
-    file.write(f"Объем воды:\t{volume:.2f} мл\n")
-print("Рецепт сохранен в файл recipe.txt")
