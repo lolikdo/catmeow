872dab7379d7d39da794b45b1a9416ca095686f8
Софья Барановская <baranovskaya.sk@edu.spbstu.ru>
2026-02-25 12:41:38 +0300

    Delete task_2_2 directory

--- task_2_2/task_2-2_1.py
+++
@@ -1,4 +0,0 @@
-course_name = "english language"
-current_course_number = "3"
-number_of_completed_tasks = "23"
-print(f"Course: {course_name}, stage: {current_course_number}, completed tasks: {number_of_completed_tasks}")
