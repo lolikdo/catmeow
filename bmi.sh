cd278be4bf23066cbcb5dcc72a91fd1a1f1acbb4
Софья Барановская <baranovskaya.sk@edu.spbstu.ru>
2026-03-06 00:17:44 +0300

    Delete bmi.sh

--- bmi.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-read -p "Введите массу (кг): " WEIGHT
-
-read -p "Введите рост (в сантиметрах): " HEIGHT_CM
-
-BMI=$(( WEIGHT * 10000 / (HEIGHT_CM * HEIGHT_CM) ))
-
-echo "Ваш индекс массы тела (ИМТ): $BMI"
