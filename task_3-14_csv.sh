ce8b466acc68867a12a6acc1d1a6f3cb7f5ec1d7
Софья Барановская <baranovskaya.sk@edu.spbstu.ru>
2026-03-06 00:20:25 +0300

    Delete task_3-14_csv.sh

--- task_3-14_csv.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo "Названия товаров:"
-awk -F "," '{print $2}' data.csv
-echo ""
-echo "Товары дороже 20:"
-awk -F "," '$3 > 20 {print $2}' data.csv
-echo ""
-echo "Общая стоимость всех товаров:"
-awk -F "," '{sum += $3} END {print sum}' data.csv
