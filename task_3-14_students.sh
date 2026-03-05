8b33388e5add70c2a8d7d3f5724fb235d025886d
Софья Барановская <baranovskaya.sk@edu.spbstu.ru>
2026-03-06 00:20:49 +0300

    Delete task_3-14_students.sh

--- task_3-14_students.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo "Только имена:"
-awk '{print $1}' students.txt
-echo ""
-echo "Только оценки:"
-awk '{print $2}' students.txt
-echo ""
-echo "Номер строки и имя:"
-awk '{print NR, $1}' students.txt
